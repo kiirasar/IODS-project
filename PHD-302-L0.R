a0ce951d0f22b1c7fb71c045b6b7bc36c713c6b1
Kiira Sarasjärvi <117264950+kiirasar@users.noreply.github.com>
2022-11-02 15:49:08 +0200

    Instructions to upload your code to GitHub (.R and .Rmd files)

---
+++ PHD-302-L0.R
@@ -0,0 +1,9 @@
+
+# To launch you code to GitHub
+
+# 1. Save your latest version from Rstudio
+# 2. Press Git (next to Environment, History, Connections, Git)
+# 3. Choose the .Rmd file you want to upload (tick it)
+# 4. Press "Commit" below the task bar e.g., "chapter1" choose the one with blue M
+# 5. Write a commit message (what you have changed)
+# 6. Press commit
